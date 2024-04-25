6b8d1323ce02048fab5a3a98496f62d36ffdd075
kartikpachori <kartikeypachori1610@gmail.com>
2024-04-25 06:24:24 +0530

    commit in past

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Chatting App',
+      title: 'Chat App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
